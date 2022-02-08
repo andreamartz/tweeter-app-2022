35e742c40e7c460c471ed558fad7e53e93b101de
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-02-08 12:01:03 -0600

    Style the tweet input and allow it to grow vertically with post length

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,31 +1,36 @@
 <template>
 	<q-page>
-		<div class="q-py-lg q-px-md">
-			<q-input
-				bottom-slots
-				v-model="newTweetContent"
-				placeholder="What's happening?"
-				counter
-				maxlength="280"
-			>
-				<template v-slot:before>
-					<q-avatar size="xl">
-						<img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-					</q-avatar>
-				</template>
-
-				<template v-slot:after>
-					<q-btn
-						:disable="!newTweetContent"
-						color="primary"
-						label="Tweet"
-						unelevated
-						rounded
-						no-caps
-					/>
-				</template>
-			</q-input>
+		<div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+			<div class="col">
+				<q-input
+					class="new-tweet"
+					bottom-slots
+					v-model="newTweetContent"
+					placeholder="What's happening?"
+					counter
+					maxlength="280"
+					autogrow
+				>
+					<template v-slot:before>
+						<q-avatar size="xl">
+							<img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+						</q-avatar>
+					</template>
+				</q-input>
+			</div>
+			<div class="col col-shrink">
+				<q-btn
+					class="q-mb-lg"
+					:disable="!newTweetContent"
+					color="primary"
+					label="Tweet"
+					unelevated
+					rounded
+					no-caps
+				/>
+			</div>
 		</div>
+		<q-separator class="divider" size="10px" color="grey-2" />
 	</q-page>
 </template>
 
@@ -41,3 +46,14 @@ export default {
 	},
 };
 </script>
+
+<style lang="sass">
+.new-tweet textarea[maxLength="280"]
+	font-size: 19px
+	line-height: 1.4
+
+.divider
+	border-top: 1px solid
+	border-bottom: 1px solid
+	border-color: $grey-4
+</style>
