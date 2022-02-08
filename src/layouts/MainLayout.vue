2018017cd33b84070eb799aec5f640da01dea56f
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-02-07 20:30:29 -0600

    Format right drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -33,8 +33,69 @@
 			</q-list>
 		</q-drawer>
 
-		<q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-			<!-- drawer content -->
+		<q-drawer show-if-above v-model="right" side="right" bordered>
+			<q-input
+				placeholder="Search Tweeter"
+				rounded
+				outlined
+				dense
+				class="q-ma-md"
+			>
+				<template v-slot:prepend>
+					<q-icon name="search" />
+				</template>
+			</q-input>
+
+			<q-list padding separator>
+				<q-item class="q-pa-md">
+					<q-item-section>
+						<q-item-label class="text-grey" overline>Education</q-item-label>
+						<q-item-label class="text-weight-bold"
+							>Something amazing happened!</q-item-label
+						>
+						<q-item-label caption
+							>Secondary line text. Lorem ipsum dolor sit amet, consectetur
+							adipiscit elit.</q-item-label
+						>
+					</q-item-section>
+
+					<q-item-section side top>
+						<q-item-label caption>5 min ago</q-item-label>
+					</q-item-section>
+				</q-item>
+				<q-item class="q-pa-md">
+					<q-item-section>
+						<q-item-label class="text-grey" overline>Education</q-item-label>
+						<q-item-label class="text-weight-bold"
+							>Something amazing happened!</q-item-label
+						>
+						<q-item-label caption
+							>Secondary line text. Lorem ipsum dolor sit amet, consectetur
+							adipiscit elit.</q-item-label
+						>
+					</q-item-section>
+
+					<q-item-section side top>
+						<q-item-label caption>5 min ago</q-item-label>
+					</q-item-section>
+				</q-item>
+				<q-item class="q-pa-md">
+					<q-item-section>
+						<q-item-label class="text-grey" overline>Education</q-item-label>
+						<q-item-label class="text-weight-bold"
+							>Something amazing happened!</q-item-label
+						>
+						<q-item-label caption
+							>Secondary line text. Lorem ipsum dolor sit amet, consectetur
+							adipiscit elit.</q-item-label
+						>
+					</q-item-section>
+
+					<q-item-section side top>
+						<q-item-label caption>5 min ago</q-item-label>
+					</q-item-section>
+				</q-item>
+			</q-list>
 		</q-drawer>
 
 		<q-page-container>
