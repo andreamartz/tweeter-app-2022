ce4f06adbb2804c3dfb2f2c200d5ca33102bad86
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-02-08 11:08:14 -0600

    Add form for new posts

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,13 +1,43 @@
 <template>
 	<q-page>
-		<h5>Home</h5>
+		<div class="q-py-lg q-px-md">
+			<q-input
+				bottom-slots
+				v-model="newTweetContent"
+				placeholder="What's happening?"
+				counter
+				maxlength="280"
+			>
+				<template v-slot:before>
+					<q-avatar size="xl">
+						<img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+					</q-avatar>
+				</template>
+
+				<template v-slot:after>
+					<q-btn
+						:disable="!newTweetContent"
+						color="primary"
+						label="Tweet"
+						unelevated
+						rounded
+						no-caps
+					/>
+				</template>
+			</q-input>
+		</div>
 	</q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-export default defineComponent({
+export default {
 	name: 'PageHome',
-});
+	data() {
+		return {
+			newTweetContent: '',
+		};
+	},
+};
 </script>
