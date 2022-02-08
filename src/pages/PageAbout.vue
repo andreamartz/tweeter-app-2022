3561e0d9ce868672e18f1919099c101bd32efdcd
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-02-08 10:43:20 -0600

    Flesh out the About page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,20 @@
 <template>
-	<q-page>
-		<h5>About</h5>
+	<q-page class="q-pa-lg">
+		<h4 class="q-mt-none q-mb-md text-weight-bold">About Tweeter</h4>
+		<div class="text-body1">
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id
+				sapiente minima temporibus veniam voluptate beatae aspernatur architecto
+				totam similique laborum numquam eligendi vitae qui odit fugiat, quas,
+				pariatur accusantium.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id
+				sapiente minima temporibus veniam voluptate beatae aspernatur architecto
+				totam similique laborum numquam eligendi vitae qui odit fugiat, quas,
+				pariatur accusantium.
+			</p>
+		</div>
 	</q-page>
 </template>
 
