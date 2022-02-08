6db48a6f714d5f5b73993f8bb2a12bc1d7a6ece0
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-02-08 14:29:48 -0600

    Style the tweets list

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -31,6 +31,36 @@
 			</div>
 		</div>
 		<q-separator class="divider" size="10px" color="grey-2" />
+		<q-list>
+			<q-item class="q-py-md">
+				<q-item-section avatar top>
+					<q-avatar>
+						<img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+					</q-avatar>
+				</q-item-section>
+
+				<q-item-section>
+					<q-item-label class="text-subtitle1">
+						<strong>Jane Doe</strong>
+						<span class="text-grey-7"> @janedoe </span>
+					</q-item-label>
+					<q-item-label class="tweet-content text-body1">
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+						reprehenderit voluptatem consectetur magnam enim repellat odit,
+						animi dolore minus sed ipsa veniam! Tempore cumque est dolorem quis
+						itaque sapiente exercitationem.
+					</q-item-label>
+					<div class="tweet-icons row justify-between q-mt-sm">
+						<q-btn flat round color="grey" size="sm" icon="far fa-comment" />
+						<q-btn flat round color="grey" size="sm" icon="fas fa-retweet" />
+						<q-btn flat round color="grey" size="sm" icon="far fa-heart" />
+						<q-btn flat round color="grey" size="sm" icon="fas fa-trash" />
+					</div>
+				</q-item-section>
+
+				<q-item-section side top> 1 min ago </q-item-section>
+			</q-item>
+		</q-list>
 	</q-page>
 </template>
 
@@ -56,4 +86,10 @@ export default {
 	border-top: 1px solid
 	border-bottom: 1px solid
 	border-color: $grey-4
+
+.tweet-content
+	white-space: pre-line
+
+.tweet-icons
+	margin-left: -5px
 </style>
