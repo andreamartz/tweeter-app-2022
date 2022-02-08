35c9481d39d2d6cb150d2472037bcd191fa1bbfd
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-02-08 17:56:26 -0600

    Edit the dummy tweets to make them unique

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -75,7 +75,7 @@ export default {
 			tweets: [
 				{
 					content:
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagittis, odio magna semper ipsum, et laoreet sapien mauris vitae arcu.',
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagittis, some different text',
 					date: 1644352412509,
 				},
 				{
